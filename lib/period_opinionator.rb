9c3ba078ce0cdd206a82bf0988899fa37a5bd932
TheNotary <no@email.plz>
2016-06-16 10:41:27 -0700

    tests pass for correcting a string

--- lib/period_opinionator.rb
+++ lib/period_opinionator.rb
@@ -1,5 +1,15 @@
 require "period_opinionator/version"
 
 module PeriodOpinionator
-  # Your code goes here...
+
+  def self.correct(text)
+    fixed_text = remove_extra_spaces_after_period(text)
+
+    { body: fixed_text,
+      diff: "blah" }
+  end
+
+  def remove_extra_spaces_after_period(text)
+    text.gsub(/\. +/, ". ")
+  end
 end
--- spec/period_opinionator_spec.rb
+++ spec/period_opinionator_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe PeriodOpinionator do
+
+  before :each do
+    @un_corrected_string = "This is a sentence.  Typography is important to get correct.    So is writing code like `this.powerful(function)`"
+    @corrected_string = "This is a sentence. Typography is important to get correct. So is writing code like `this.powerful(function)`"
+  end
+
   it 'has a version number' do
     expect(PeriodOpinionator::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'corrects a basic string of text' do
+    # binding.pry
+    result = PeriodOpinionator.correct(@un_corrected_string)
+    expect(result[:body]).to eq(@corrected_string)
   end
 end
